7a4c2a6cb09980c55734dc36abdb91422497e251
Jeison De los Santos <112901441+Jeisondls@users.noreply.github.com>
2022-09-13 00:29:08 -0400

    Add files via upload

---
+++ mokepon!/js/mokepon.js
@@ -0,0 +1,23 @@
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+}
+
+function seleccionarMascotaJugador() {
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    
+    if(inputHipodoge.checked) {
+        alert('Seleccionaste a Hipodoge')
+    } else if (inputCapipepo.checked) {
+        alert('Seleccionaste a Capipepo')
+    } else if (inputRatigueya.checked) {
+        alert('Seleccionaste a Ratigueya')
+    } else {
+        alert('Selecciona una mascota')
+    }
+    
+}
+
+window.addEventListener('load', iniciarJuego)
